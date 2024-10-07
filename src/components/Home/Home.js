a035807bee03d96726e15bf3b33733746d23b80f
IkrameHafsi <147140863+IkrameHafsi@users.noreply.github.com>
2024-10-07 13:35:13 +0200

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,15 +14,15 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
+                Welcome!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
+                  🌱
                 </span>
               </h1>
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Badreddine saadioui</strong>
+                <strong className="main-name"> Ikrame Hafsi</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
